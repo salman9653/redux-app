b02a88e57bd8ca6e8242e4a10f4aba9b4bc7f55d
Salman <salman9915189734@gmail.com>
2022-03-17 23:38:40 +0530

    project setup

---
+++ src/Shop.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+    return (
+        <div className='container text-center mt-5'>
+            <h1>Withdraw / Deposit</h1>
+            <button className='btn btn-danger m-3 px-5'> - </button>
+            <button className='btn btn-success  m-3 px-5'> + </button>
+
+        </div>
+    )
+}
+
+export default Shop
